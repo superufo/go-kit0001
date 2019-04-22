a91eaa4cdf137918e5b7915fdc0b6963a6fda434
mike <mike.liu@joyte.ch>
2019-04-22 15:35:44 +0700

    hystrix

--- hystrix/gateway/gateway.go
+++ hystrix/gateway/gateway.go
@@ -3,12 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
 	"net/http"
-	"net/http/httputil"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/go-kit/kit/log"
@@ -73,20 +70,22 @@ func main(){
 	}
 
 	//创建反向代理   zipkinTracer 传递
-	proxy := NewReverseProxy(consulClient,zipkinTracer,logger)
+	//proxy := NewReverseProxy(consulClient,zipkinTracer,logger)
+	hystrixRouter:= Routes(consulClient,zipkinTracer,"Circuit Breaker:Service unavailable", logger)
+
 
 	tags := map[string]string{
 		"component":"gateway_server",
 	}
 
 	//NewServerMiddleware(t *zipkin.Tracer, options ...ServerOption) func(http.Handler) http.Handler
-	//装饰器包裹
+	//装饰器包裹  NewServerMiddleware returns a http.Handler middleware with Zipkin tracing.
 	handle := zipkinhttpsvr.NewServerMiddleware(
 		zipkinTracer,
 		zipkinhttpsvr.SpanName("gateway"),
 		zipkinhttpsvr.TagResponseSize(true),
 		zipkinhttpsvr.ServerTags(tags),
-		)(proxy)
+		)(hystrixRouter)
 
 
 
@@ -109,50 +108,50 @@ func main(){
 	logger.Log("exit", <-errc)
 }
 
-func NewReverseProxy(client *api.Client, zikkinTracer *zipkin.Tracer,logger log.Logger) *httputil.ReverseProxy {
-	//创建Direct
-	director := func(req *http.Request)  {
-		//查询原始请求路径，如：/arithmetic/calculate/10/5
-		reqPath := req.URL.Path
-		if reqPath==""{
-			return
-		}
-
-		//按照分隔符'/'对路径进行分解，获取服务名称serviceName
-		pathArray := strings.Split(reqPath,"/")
-		serviceName := pathArray[1]
-
-		//调用consul api查询serviceName的服务实例列表
-		result,_,err := client.Catalog().Service(serviceName,"",nil)
-		if err!=nil {
-			logger.Log("ReverseProxy failed", "query service instace error", err.Error())
-			return
-		}
-
-		if len(result) == 0 {
-			logger.Log("ReverseProxy failed", "no such service instance", serviceName)
-			return
-		}
-
-		//重新组织请求路径 去掉服务部分
-		destPath := strings.Join(pathArray[2:],"/")
-
-		//随机选择一个服务实例
-		tgt := result[rand.Int()%len(result)]
-		logger.Log("service id", tgt.ServiceID)
-
-		req.URL.Scheme = "http"
-		req.URL.Host = fmt.Sprintf("%s:%d",tgt.ServiceAddress,tgt.ServicePort)
-		req.URL.Path = "/"+destPath
-	}
-
-	/**gateway接收请求后，会创建一个span，其中的traceId将作为本次请求的唯一编号，
-	gateway必须把这个traceId“告诉”算术运算服务，算术运算服务才能为该请求持续记录追踪信息。
-	在ReverseProxy中能够完成这一任务的就是Transport，我们可以使用zipkin-go的middleware/http包
-	提供的NewTransport替换系统默认的http.DefaultTransport
-	***/
-	rounndTrip, _ := zipkinhttpsvr.NewTransport(zikkinTracer,zipkinhttpsvr.TransportTrace(true))
-
-	//Director func(*http.Request) 构造新的 *http.Request
-	return &httputil.ReverseProxy{Director:director,Transport:rounndTrip}
-}
+//func NewReverseProxy(client *api.Client, zikkinTracer *zipkin.Tracer,logger log.Logger) *httputil.ReverseProxy {
+//	//创建Direct
+//	director := func(req *http.Request)  {
+//		//查询原始请求路径，如：/arithmetic/calculate/10/5
+//		reqPath := req.URL.Path
+//		if reqPath==""{
+//			return
+//		}
+//
+//		//按照分隔符'/'对路径进行分解，获取服务名称serviceName
+//		pathArray := strings.Split(reqPath,"/")
+//		serviceName := pathArray[1]
+//
+//		//调用consul api查询serviceName的服务实例列表
+//		result,_,err := client.Catalog().Service(serviceName,"",nil)
+//		if err!=nil {
+//			logger.Log("ReverseProxy failed", "query service instace error", err.Error())
+//			return
+//		}
+//
+//		if len(result) == 0 {
+//			logger.Log("ReverseProxy failed", "no such service instance", serviceName)
+//			return
+//		}
+//
+//		//重新组织请求路径 去掉服务部分
+//		destPath := strings.Join(pathArray[2:],"/")
+//
+//		//随机选择一个服务实例
+//		tgt := result[rand.Int()%len(result)]
+//		logger.Log("service id", tgt.ServiceID)
+//
+//		req.URL.Scheme = "http"
+//		req.URL.Host = fmt.Sprintf("%s:%d",tgt.ServiceAddress,tgt.ServicePort)
+//		req.URL.Path = "/"+destPath
+//	}
+//
+//	/**gateway接收请求后，会创建一个span，其中的traceId将作为本次请求的唯一编号，
+//	gateway必须把这个traceId“告诉”算术运算服务，算术运算服务才能为该请求持续记录追踪信息。
+//	在ReverseProxy中能够完成这一任务的就是Transport，我们可以使用zipkin-go的middleware/http包
+//	提供的NewTransport替换系统默认的http.DefaultTransport
+//	***/
+//	rounndTrip, _ := zipkinhttpsvr.NewTransport(zikkinTracer,zipkinhttpsvr.TransportTrace(true))
+//
+//	//Director func(*http.Request) 构造新的 *http.Request
+//	return &httputil.ReverseProxy{Director:director,Transport:rounndTrip}
+//}
--- hystrix/readme.md
+++ hystrix/readme.md
@@ -1,9 +1,12 @@
-
 main.exe  -consul.host 47.112.111.171 -consul.port 8500  -service.host 110.235.246.150   -service.port 9550          
 main.exe   -consul.host 47.112.111.171  -consul.port 8500
 
 
-zipkin.exe -consul.host 47.112.111.171 -consul.port 8500  --zipkin.url http://47.112.111.171:9411/zipkin/api/v2/spans
+zipkin.exe -consul.host 47.112.111.171 -consul.port 8500  --zipkin.url http://47.112.111.171:9411/zipkin/api/v2/spans    
 
 register.exe -consul.host 47.112.111.171 -consul.port 8500  -service.host 110.235.246.150   -service.port 9550  
---zipkin.url http://47.112.111.171:9411/zipkin/api/v2/spans
+--zipkin.url http://47.112.111.171:9411/zipkin/api/v2/spans    
+
+
+http://47.112.111.171:8181/hystrix
+http://127.0.0.1:8880/arithmetic/calculate/Add/9851/1245821
